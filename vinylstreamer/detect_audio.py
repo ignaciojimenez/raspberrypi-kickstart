780277e64a82b428b6b386f1d2f85dc882a9e5b4
Ignacio Jimenez <i.jimenezpi@gmail.com>
2021-03-31 23:46:30 +0200

    Modified to direct output to stdout and get  sexecution history

--- vinylstreamer/detect_audio.py
+++ vinylstreamer/detect_audio.py
@@ -1,12 +1,12 @@
-from sys import byteorder
 from array import array
 import pyaudio
 import subprocess
+import sys
 
 THRESHOLD = 350
 CHUNK_SIZE = 2048
 FORMAT = pyaudio.paInt16
-RATE = 44100
+RATE = 48000
 hifipi_ip="10.30.80.100"
 
 
@@ -32,19 +32,19 @@ def listen():
         stream = p.open(format=FORMAT, channels=2, rate=RATE, input=True, output=False, frames_per_buffer=CHUNK_SIZE)
         # little endian, signed short
         snd_data = array('h', stream.read(CHUNK_SIZE))
-        if byteorder == 'big':
+        if sys.byteorder == 'big':
             snd_data.byteswap()
         r.extend(snd_data)
 
         if is_silent(snd_data):
             if snd_started:
-                print(f"stop stream: {max(snd_data)}")
+                sys.stdout.write(f"stop stream. Volume: {max(snd_data)}\n")
                 subprocess.run(["mpc", f"--host={hifipi_ip}","stop"])
                 snd_started = False
         elif not snd_started:
-            print(f"start stream: {max(snd_data)}")
+            sys.stdout.write(f"start stream. Volume: {max(snd_data)}\n")
             # TODO: check mpc --host=10.30.80.100 playlist should return My Turntable, if not --clear and add again
-            # TODO: create function to check if playlist loaded and other issues?
+            # TODO: create function to check if playlist loaded and detect other issues?
             subprocess.run(["mpc", f"--host={hifipi_ip}","play"])
             snd_started = True
 
